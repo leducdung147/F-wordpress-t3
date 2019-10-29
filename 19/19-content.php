6f462935050a38b148a3984d261845625a288867
lethanh99 <lethanh131199@gmail.com>
2019-10-29 10:55:07 +0700

    le thanh

---
+++ 19/19-content.php
@@ -0,0 +1,9 @@
+<div class="type-19 ">
+            <div class="container">
+                <div class="row">  
+                    <h3 class="wellcom">We are going to...</h3>
+                    <h2 class="love">Celebrate Our Love</h2>             
+               </div>
+            </div>
+</div>
+
---
+++ 8/8-content.php
@@ -0,0 +1,22 @@
+<div class="type-8">
+    <div class="container footer-bottom">
+        <div class="row">
+            <div class="col-xs-5 col-md-5 col-lg-5">
+              <h4 class="Copyright">© Copyright 2017, Built by Themes Awesome</h4> 
+            </div>
+            <div class="col-xs-3 col-md-3 col-lg-3">
+                <img src="./images/nikah-logo-final-70.png" alt="logo">
+            </div>
+            <div class="col-xs-4 col-md-4 col-lg-4">
+                <ul class="iconxahoi">
+                    <li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a> </li>
+                    <li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                     <li><a href="#" title="Pinterest"><i class="fa fa-pinterest-p"></i></a></li>
+                     <li><a href="#" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        
+       </div>
+   
+</div>
+
